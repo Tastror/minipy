ef99b1890f4f455913ef29a1ec1658d0d8f786bc
Tastror <yccozy2001@163.com>
2023-06-24 17:58:41 +0800

    add normal ir

--- src/ir.cpp
+++ src/ir.cpp
@@ -9,13 +9,40 @@
 #include "symbol.h"
 #include "ir.h"
 
+Reg::Reg() {
+    id = 0;
+}
+
+int Reg::get_int() {
+    return ++id;
+}
+
+std::string Reg::get_str() {
+    return std::to_string(++id);
+}
+
 std::string ir_sentence::to_string() const {
     return "";
+}
+
+void sausgi(AstNode*& astnode_now, SymbolTable& sym_table, std::vector<ir_sentence>& ir_vec) {
+    if (astnode_now == nullptr) return;
+    switch (astnode_now->type) {
+    case astnode_type::statements:
+        for (auto i : astnode_now->sons) {
+            sausgi(astnode_now, sym_table, ir_vec);
+        }
+        break;
+    case astnode_type::bin_op_add:
+        break;
+    }
 }
 
 std::vector<ir_sentence> search_astnode_update_symboltable_generate_ir(
     AstNode*& ast_root, SymbolTable& sym_table
 ) {
     std::vector<ir_sentence> res;
+    Reg reg;
+    sausgi(ast_root, sym_table, res);
     return res;
 }
--- src/ir.h
+++ src/ir.h
@@ -10,7 +10,47 @@
 #include "parser.h"
 #include "symbol.h"
 
+class Reg {
+private:
+    int id;
+public:
+    Reg();
+    int get_int();
+    std::string get_str();
+};
+
+enum class ir_op_type {
+    error,
+    label_hint,  // label_name1:  # type1 == label, but not show here; number should be <label>:num:
+    alloca,  // name1 = alloca type1
+    load,  // name1 = load type1, ptr_type2 name2
+    store,  // store type1 name1, ptr_type2 name2
+    add, sub, mul, div, rems,  // name1 = <op> type1 name2, name3
+    uadd, usub, umul, udiv, urems,  // name1 = <op> type1 name2, name3
+    fadd, fsub, fmul, fdiv,  // name1 = <op> type1 name2, name3
+    eq, ne, ugt, uge, ult, ule, sgt, sge, slt, sle,  // name1 = icmp <op> type1 name2, name3
+    br_1,  // br label_type1 name1
+    br_3,  // br type1 name1, label_type2 name2, label_type3 name3
+    ret,  // ret type1 name1
+    switch_begin,  // switch type1 name1, label_type2 name2 [  # this label is for default
+    switch_inner,  // type1 value1, label_type2 name2 
+    switch_end,  // ]
+    func_begin,  // define return_value[type1] \@func_name[name1] (type name, ...) #0 {
+    func_end,  // } 
+};
+
+enum class ir_data_type {
+    none,
+    instant,  // instant number
+    label, voids,
+    i32, i64, floats, doubles,
+    i32_p, i64_p, float_p, double_p,  // pointer
+};
+
 struct ir_sentence {
+    ir_op_type op_type;
+    std::vector<std::string> names;
+    std::vector<ir_data_type> types;
     std::string to_string() const;
 };
 
